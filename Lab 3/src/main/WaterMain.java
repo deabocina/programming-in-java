59c5a7de699c36e7999938933f323cbd4caedfb4
Dea Bočina <140092973+deabocina@users.noreply.github.com>
2023-11-17 19:55:38 +0100

    Update WaterMain.java

--- Lab 3/src/main/WaterMain.java
+++ Lab 3/src/main/WaterMain.java
@@ -1,12 +1,9 @@
 package main;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 import java.util.*;
 
 public class WaterMain {
-    private static final Logger logger = Logger.getLogger(WaterMain.class.getName()); // Define the logger
-
     public static void main(String[] args) throws IOException {
         WaterMeasure connect = new WaterMeasure("tcp://localhost:1883", "iMeasure");
 
